d97f224fa110e870610e68d89fe48e35e4f283ee
1406MJM <3330kys@dsm.hs.kr>
2021-10-27 19:30:58 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-const name:string =  'jeongmin'
-const age:number = 24;
-const gender:string =  'male'
-
-const sayHi = (name? : string, age? : number, gender?: string) : string => { // arguments 중 arguments 뒤에 ?를 붙여서 선택적으로 매개변수를 쓸수 있게한다.
-    return `hello ${name}, you are ${age}, you are a ${gender}`;   
+class human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender?:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
-console.log(sayHi(name,age));
+const lnn = new human ("lnn", 17, "male");
+
+const sayHi = (person : human) : string => { // arguments 중 arguments 뒤에 ?를 붙여서 선택적으로 매개변수를 쓸수 있게한다.
+    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;   
+}
+console.log(sayHi(lnn));
 export{};
