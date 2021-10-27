3fe6a0d1f47d5b33a875eeed3018acae4c69df95
1406MJM <3330kys@dsm.hs.kr>
2021-10-27 12:10:36 +0900

    arguments

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('hello');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'jeongmin';
+const age = 24;
+const gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
