dae395028b4816819a53da382f235e6705a507aa
1406MJM <3330kys@dsm.hs.kr>
2021-11-01 19:59:52 +0900

    creating a block

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-class human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age:number, gender?:string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp:number;
+    constructor(index: number, hash: string, previousHash: string, data:string,  timestamp: number){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const lnn = new human ("lnn", 17, "male");
 
-const sayHi = (person : human) : string => { // arguments 중 arguments 뒤에 ?를 붙여서 선택적으로 매개변수를 쓸수 있게한다.
-    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;   
-}
-console.log(sayHi(lnn));
+const genesisBlock:Block = new Block(0, "2020202020202", "", "Hello", 123456);
+let blockchain: [Block] = [genesisBlock];
+
+console.log(genesisBlock);
 export{};
